c9df08076b9fb1aeca2ee10700fb0d1d63e782a4
CornLiu <cornliu@zju.edu.cn>
2022-11-23 15:37:34 +0800

    2022/11/23

---
+++ 322. Coin Change/dynamic_programming.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; ++i) {
+            for (int j = 0; j < coins.size(); ++j) {
+                if (coins[j] <= i) {
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> coins{186,419,83,408};
+    int result = solution.coinChange(coins, 6249);
+    cout << result;
+    return 0;
+}
