985ed8996d1b490be753be9f85f91e53915daae8
CornLiu <74982782+oldcorn26@users.noreply.github.com>
2023-02-27 17:12:38 +0800

    Update dfs.cpp

--- 001-100/037. Sudoku Solver/dfs.cpp
+++ 001-100/037. Sudoku Solver/dfs.cpp
@@ -1,73 +1,47 @@
 #include <iostream>
 #include <vector>
-#include <bitset>
 
 using namespace std;
 
 class Solution {
 private:
-    vector<bitset<9>> rows, cols, cells;
+    int stlNum{0};
+    vector<int> col{0, 0, 0, 0, 0, 0, 0, 0, 0};
+    vector<int> row{0, 0, 0, 0, 0, 0, 0, 0, 0};
+    vector<int> bloc{0, 0, 0, 0, 0, 0, 0, 0, 0};
 public:
     void solveSudoku(vector<vector<char>> &board) {
-        int &&settledNum{initialState(board)};
-        dfs(board, settledNum);
-    }
-
-    bool dfs(vector<vector<char>> &board, int stlNum) {
-        if (stlNum == 81) return true;
-
-        vector<int> &&next{getNext(board)};
-        bitset<9> &&bits{getPossibleStatus(next[0], next[1])};
-        for (int idx{0}; idx < 9; ++idx) {
-            if (!bits.test(idx)) continue;
-            fillNum(next[0], next[1], idx, true);
-            board[next[0]][next[1]] = idx + '1';
-            if (dfs(board, stlNum + 1)) return true;
-            board[next[0]][next[1]] = '.';
-            fillNum(next[0], next[1], idx, false);
-        }
-        return false;
-    }
-
-    bitset<9> getPossibleStatus(int x, int y) {
-        return ~(rows[x] | cols[y] | cells[x / 3 * 3 + y / 3]);
-    }
-
-    vector<int> getNext(vector<vector<char>> &board) {
-        int minCnt{10};
-        vector<int> res(2);
         for (int i{0}; i < 9; ++i) {
             for (int j{0}; j < 9; ++j) {
-                if (board[i][j] != '.') continue;
-                bitset<9> &&crt{getPossibleStatus(i, j)};
-                if (crt.count() >= minCnt) continue;
-                res[0] = i, res[1] = j;
-                minCnt = crt.count();
+                if (board[i][j] == '.') continue;
+                int blcIdx{i / 3 * 3 + j / 3};
+                int mask{1 << (board[i][j] - '1')};
+                row[i] |= mask, col[j] |= mask, bloc[blcIdx] |= mask;
+                stlNum++;
             }
         }
-        return res;
+        dfs(board, 0, 0);
     }
 
-    void fillNum(int x, int y, int n, bool fillFlag) {
-        rows[x][n] = cols[y][n] = cells[x / 3 * 3 + y / 3][n] = fillFlag ? 1 : 0;
-    }
+    bool dfs(vector<vector<char>> &board, int x, int y) {
+        if (x == 9) return stlNum == 81;
+        if (y == 9) return dfs(board, x + 1, 0);
+        if (board[x][y] != '.') return dfs(board, x, y + 1);
 
-    int initialState(vector<vector<char>> &board) {
-        int stlNum{0};
-        rows = vector<bitset<9>>(9, bitset<9>());
-        cols = vector<bitset<9>>(9, bitset<9>());
-        cells = vector<bitset<9>>(9, bitset<9>());
-        for (int i = 0; i < 9; ++i) {
-            for (int j = 0; j < 9; ++j) {
-                int idx{board[i][j] - '1'};
-                if (idx >= 0) {
-                    rows[i][idx] = cols[j][idx] = 1;
-                    cells[i / 3 * 3 + j / 3][idx] = 1;
-                    ++stlNum;
-                }
-            }
+        int blcIdx{x / 3 * 3 + y / 3};
+        for (int k{0}; k < 9; ++k) {
+            int mask{1 << k};
+            if (row[x] & mask || col[y] & mask || bloc[blcIdx] & mask) continue;
+            board[x][y] = k + '1';
+            row[x] |= mask, col[y] |= mask, bloc[blcIdx] |= mask;
+            stlNum++;
+            if (dfs(board, x, y + 1)) return true;
+            board[x][y] = '.';
+            row[x] ^= mask, col[y] ^= mask, bloc[blcIdx] ^= mask;
+            stlNum--;
         }
-        return stlNum;
+
+        return false;
     }
 };
 
