29399b4e381d537dbfd15352d4a9a6c37727e0c1
CornLiu <74982782+oldcorn26@users.noreply.github.com>
2023-03-10 01:19:42 +0800

    2023/3/10

--- 301-400/321. Create Maximum Number/monotonic_stack.cpp
+++ 301-400/321. Create Maximum Number/monotonic_stack.cpp
@@ -1,78 +1,61 @@
 #include <iostream>
-#include <stack>
 #include <vector>
 
 using namespace std;
 
 class Solution {
-private:
-    vector<int> res;
 public:
-    vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) {
-        int &&deleteNum = nums1.size() + nums2.size() - k;
-        int i = deleteNum <= nums2.size() ? 0 : deleteNum - nums2.size();
-        res.resize(k);
-        for (; i <= deleteNum && i <= nums1.size(); ++i) {
-            vector<int> &&temp1 = removeKdigits(nums1, i);
-            vector<int> &&temp2 = removeKdigits(nums2, deleteNum - i);
-            findLargeNumber(res, temp1, temp2);
+    vector<int> maxNumber(vector<int> &nums1, vector<int> &nums2, int k) {
+        vector<int> ret(k);
+        for (int i{0}; i <= k; ++i) {
+            if (nums1.size() < i || nums2.size() < k-i) continue;
+            vector<int> &&mono1{monotonicStack(nums1, i)};
+            vector<int> &&mono2{monotonicStack(nums2, k - i)};
+            mergeVec(ret, mono1, mono2);
         }
-        return res;
+        return ret;
     }
 
-    vector<int> removeKdigits(const vector<int> &nums, int k) {
-        if (!k) return nums;
-
-        int deleteNum{k}, &&resNum{static_cast<int>(nums.size() - k)};
-        vector<int> res(resNum);
-        if (k >= nums.size()) return {};
-
-        stack<char> monStack;
-        for (const int &i: nums) {
-            while (!monStack.empty() && monStack.top() < i && deleteNum > 0) {
-                --deleteNum;
-                monStack.pop();
+    void mergeVec(vector<int> &ret, vector<int> &nums1, vector<int> &nums2) {
+        char flag{};
+        int i1{0}, i2{0};
+        for (int i{0}; i < ret.size(); ++i) {
+            int comFlag{compare(nums1, i1, nums2, i2)};
+            int max{comFlag > 0 ? nums1[i1++] : nums2[i2++]};
+            if (flag) ret[i] = max;
+            else if (ret[i] > max) return;
+            else if (ret[i] < max) {
+                ret[i] = max;
+                flag = 1;
             }
-            monStack.emplace(i);
-        }
-        while (monStack.size() > resNum) monStack.pop();
-        for (int &&i = resNum - 1; i >= 0; --i) {
-            res[i] = monStack.top();
-            monStack.pop();
-        }
-        return res;
-    }
-
-    void findLargeNumber(vector<int> &res, vector<int> &candi1, vector<int> &candi2) {
-        char flag{0};
-        int resIdx{0}, candiIdx1{0}, candiIdx2{0};
-        while (resIdx < res.size()) {
-            int max;
-            if (compare(candi1, candiIdx1, candi2, candiIdx2) > 0) max = candi1[candiIdx1++];
-            else max = candi2[candiIdx2++];
-            if (flag) res[resIdx] = max;
-            else {
-                if (res[resIdx] > max) return;
-                else if (max > res[resIdx]) {
-                    flag = 1;
-                    res[resIdx] = max;
-                }
-            }
-            ++resIdx;
         }
     }
 
-    int compare(vector<int>& subsequence1, int index1, vector<int>& subsequence2, int index2) {
-        int x = subsequence1.size(), y = subsequence2.size();
-        while (index1 < x && index2 < y) {
-            int difference = subsequence1[index1] - subsequence2[index2];
-            if (difference != 0) {
-                return difference;
-            }
-            index1++;
-            index2++;
+    int compare(vector<int> &one, int index1, vector<int> &two, int index2) {
+        int size1 = one.size(), size2 = two.size();
+        while (index1 < size1 && index2 < size2) {
+            int tag{one[index1++] - two[index2++]};
+            if (tag != 0) return tag;
         }
-        return (x - index1) - (y - index2);
+        return (size1 - index1) - (size2 - index2);
+    }
+
+    vector<int> monotonicStack(vector<int> &num, int len) {
+        if (len == 0) return {};
+        else if (len == num.size()) return num;
+        int deleteNum = num.size() - len;
+        int retIdx{1};
+        vector<int> ret(len);
+        ret[0] = num[0];
+        for (int i{1}; i < num.size(); ++i) {
+            while (retIdx && deleteNum > 0 && ret[retIdx-1] < num[i]) {
+                deleteNum--;
+                retIdx--;
+            }
+            if (retIdx < len) ret[retIdx++] = num[i];
+            else deleteNum--;
+        }
+        return ret;
     }
 };
 
