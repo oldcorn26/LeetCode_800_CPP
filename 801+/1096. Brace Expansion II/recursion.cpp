526f940002ebea1dd61c0c25411146cfad782826
CornLiu <74982782+oldcorn26@users.noreply.github.com>
2023-03-13 14:58:50 +0800

    2023/3/13_1

---
+++ 801+/1096. Brace Expansion II/recursion.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> braceExpansionII(string expression) {
+        dfs(expression);
+        return vector<string>(s.begin(), s.end());
+    }
+
+private:
+    set<string> s;
+
+    void dfs(string exp) {
+        int j = exp.find_first_of('}');
+        if (j == string::npos) {
+            s.insert(exp);
+            return;
+        }
+        int i = exp.rfind('{', j);
+        string a = exp.substr(0, i);
+        string c = exp.substr(j + 1);
+        stringstream ss(exp.substr(i + 1, j - i - 1));
+        string b;
+        while (getline(ss, b, ',')) {
+            dfs(a + b + c);
+        }
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
