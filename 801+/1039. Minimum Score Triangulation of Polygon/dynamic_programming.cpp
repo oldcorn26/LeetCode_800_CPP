3c333ed809759f5d9dcab40456cc43c823908390
CornLiu <74982782+oldcorn26@users.noreply.github.com>
2023-04-02 17:16:34 +0800

    2023/4/2

---
+++ 801+/1039. Minimum Score Triangulation of Polygon/dynamic_programming.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int minScoreTriangulation(vector<int>& values) {
+        unordered_map<int, int> memo;
+        int n = values.size();
+        function<int(int, int)> dp = [&](int i, int j) -> int {
+            if (i + 2 > j) {
+                return 0;
+            }
+            if (i + 2 == j) {
+                return values[i] * values[i + 1] * values[j];
+            }
+            int key = i * n + j;
+            if (!memo.count(key)) {
+                int minScore = INT_MAX;
+                for (int k = i + 1; k < j; k++) {
+                    minScore = min(minScore, values[i] * values[k] * values[j] + dp(i, k) + dp(k, j));
+                }
+                memo[key] = minScore;
+            }
+            return memo[key];
+        };
+        return dp(0, n - 1);
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> values{1,3,1,4,1,5};
+    int result = solution.minScoreTriangulation(values);
+    cout << result;
+    return 0;
+}
