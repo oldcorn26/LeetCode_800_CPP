e944a0cb98df853126db6553c2384745357cd8e4
CornLiu <74982782+oldcorn26@users.noreply.github.com>
2023-06-25 21:38:00 +0800

    2023/6/25_2

--- 001-100/056. Merge Intervals/iteration.cpp
+++ 001-100/056. Merge Intervals/iteration.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int flag = 0, idx;
-        map<int, int> map;
-        vector<vector<int>> res;
-
+        sort(intervals.begin(), intervals.end());
+        int left{intervals[0][0]}, right{intervals[0][1]};
+        vector<vector<int>> ret;
         for (auto &vec: intervals) {
-            //one left equals 1, one right equals -1.
-            ++map[vec[0]];
-            --map[vec[1]];
+            if (vec[0] > right) {
+                ret.push_back({left, right});
+                left = vec[0];
+                right = vec[1];
+            } else {
+                right = max(right, vec[1]);
+            }
         }
-
-        for (auto &&iter = map.begin(); iter != map.end(); ++iter) {
-            if (!flag) idx = iter->first;
-            flag += iter->second;
-            if (!flag) res.push_back({idx, iter->first});
-        }
-
-        return res;
+        ret.push_back({left, right});
+        return ret;
     }
 };
 
