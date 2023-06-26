5ecbb6d7a62ea26033571a766a402a12609e26cf
CornLiu <74982782+oldcorn26@users.noreply.github.com>
2023-06-26 14:26:27 +0800

    2023/6/26_3

--- 001-100/079. Word Search/dfs.cpp
+++ 001-100/079. Word Search/dfs.cpp
@@ -5,53 +5,32 @@ using namespace std;
 
 class Solution {
 private:
-    int height, width;
+    int m, n;
 public:
     bool exist(vector<vector<char>> &board, string word) {
-        height = board.size(); width = board[0].size();
-        if (word.size() > height * width) return false;
-
-        for (int i = 0; i < height; ++i) {
-            for (int j = 0; j < width; ++j) {
-                if (board[i][j] != word[0]) continue;
-                if (dfs(i * width + j, board, 0, word)) return true;
+        m = board.size(), n = board[0].size();
+        for (int i{}; i < m; ++i) {
+            for (int j{}; j < n; ++j) {
+                if (word[0] != board[i][j])
+                    continue;
+                if (dfs(board, i, j, word, 0))
+                    return true;
             }
         }
-
         return false;
     }
 
-    bool dfs(int &&boardIdx, vector<vector<char>> &board, int &&wordIdx, const string &word) {
-        int &&x = boardIdx / width, &&y = boardIdx % width;
-
-        if (board[x][y] != word[wordIdx]) return false;
-        if (wordIdx == word.size() - 1) return true;
-        else {
-            if (x && board[x - 1][y] == word[wordIdx + 1]) {
-                board[x][y] -= 60;
-                bool &&temp = dfs(boardIdx - width, board, wordIdx + 1, word);
-                if (temp) return true;
-                else board[x][y] +=60;
-            }
-            if (y != width - 1 && board[x][y + 1] == word[wordIdx + 1]) {
-                board[x][y] -= 60;
-                bool &&temp = dfs(boardIdx + 1, board, wordIdx + 1, word);
-                if (temp) return true;
-                else board[x][y] +=60;
-            }
-            if (x != height - 1 && board[x + 1][y] == word[wordIdx + 1]) {
-                board[x][y] -= 60;
-                bool &&temp = dfs(boardIdx + width, board, wordIdx + 1, word);
-                if (temp) return true;
-                else board[x][y] += 60;
-            }
-            if (y && board[x][y - 1] == word[wordIdx + 1]) {
-                board[x][y] -= 60;
-                bool &&temp = dfs(boardIdx - 1, board, wordIdx + 1, word);
-                if (temp) return true;
-                else board[x][y] += 60;
-            }
-        }
+    bool dfs(vector<vector<char>> &board, int x, int y, string &word, int wIdx) {
+        if (wIdx == word.size())
+            return true;
+        if (x >= m || y >= n || x < 0 || y < 0 || word[wIdx] != board[x][y])
+            return false;
+        char backUp{board[x][y]};
+        board[x][y] = 0;
+        if (dfs(board, x + 1, y, word, wIdx + 1) || dfs(board, x, y + 1, word, wIdx + 1) ||
+            dfs(board, x - 1, y, word, wIdx + 1) || dfs(board, x, y - 1, word, wIdx + 1))
+            return true;
+        board[x][y] = backUp;
         return false;
     }
 };
