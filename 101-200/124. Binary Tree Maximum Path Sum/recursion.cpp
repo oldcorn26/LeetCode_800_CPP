9dadea743b625a49a22d3ee702793f56abc7b8b3
CornLiu <74982782+oldcorn26@users.noreply.github.com>
2023-07-03 23:00:39 +0800

    2023/7/3

--- 101-200/124. Binary Tree Maximum Path Sum/recursion.cpp
+++ 101-200/124. Binary Tree Maximum Path Sum/recursion.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -17,38 +18,28 @@ struct TreeNode {
 };
 
 class Solution {
-    int max = INT32_MIN;
+private:
+    int maxLen{INT32_MIN};
 public:
-    int maxPathSum(TreeNode* root) {
-        rootAsTerminal(root);
-        return max;
+    int maxPathSum(TreeNode *root) {
+        helper(root);
+        return maxLen;
     }
 
-    int rootAsTerminal(TreeNode *root) {
-        if (!root) return 0;
-
-        int &&left = rootAsTerminal(root->left);
-        int &&right = rootAsTerminal(root->right);
-        const int &bigger = left < right ? right : left;
-        if (left <= 0 && right <= 0) {
-            max = root->val > max ? root->val : max;
-            return root->val > 0 ? root->val : 0;
-        } else if (left <= 0 || right <= 0) {
-            int &&temp = root->val + bigger;
-            if (temp > bigger) {max = temp > max ? temp : max; return temp;}
-            else if (temp > 0) return temp;
-            else return 0;
-        } else {
-            max = left + right + root->val > max ? left + right + root->val : max;
-            return bigger + root->val > 0 ? bigger + root->val : 0;
-        }
+    int helper(TreeNode *root) {
+        if (!root)
+            return 0;
+        int left{helper(root->left) + root->val};
+        int right{helper(root->right) + root->val};
+        maxLen = max(maxLen, max(left, right));
+        maxLen = max(maxLen, left + right - root->val);
+        return left < 0 && right < 0 ? 0 : max(left, right);
     }
 };
 
 int main() {
     Solution solution;
     TreeNode *head = new TreeNode(1, new TreeNode(2), nullptr);
-//    head->right = new TreeNode(20, 15, 7);
     int result = solution.maxPathSum(head);
     cout << result;
     return 0;
