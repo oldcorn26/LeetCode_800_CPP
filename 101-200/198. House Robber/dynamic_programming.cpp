9b606f89e414f4181219ad66338647629cb04367
CornLiu <74982782+oldcorn26@users.noreply.github.com>
2023-07-06 00:03:00 +0800

    2023/7/6

--- 101-200/198. House Robber/dynamic_programming.cpp
+++ 101-200/198. House Robber/dynamic_programming.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int dp[nums.size() + 1];
-        dp[0] = 0; dp[1] = nums[0];
-
-        for (int i = 1; i < nums.size(); ++i) {
-            dp[i + 1] = nums[i] + dp[i - 1] > dp[i] ? nums[i] + dp[i - 1] : dp[i];
+        int size = nums.size();
+        vector<vector<int>> dp(size, vector<int>(2));
+        dp[0] = {0, nums[0]};
+        for (int i{1}; i < size; ++i) {
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1]);
+            dp[i][1] = dp[i-1][0] + nums[i];
         }
-
-        return dp[nums.size()];
+        return max(dp[size-1][0], dp[size-1][1]);
     }
 };
 
