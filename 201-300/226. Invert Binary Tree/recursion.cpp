95ce7d13786c6ab327a4396500d28afd52ca6a4e
CornLiu <74982782+oldcorn26@users.noreply.github.com>
2023-07-09 16:30:02 +0800

    2023/7/9_3

--- 201-300/226. Invert Binary Tree/recursion.cpp
+++ 201-300/226. Invert Binary Tree/recursion.cpp
@@ -17,11 +17,9 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if (!root) return nullptr;
-
-        TreeNode *temp = root->left;
-        root->left = root->right;
-        root->right = temp;
+        if (!root)
+            return root;
+        swap(root->left, root->right);
         invertTree(root->left);
         invertTree(root->right);
         return root;
