45e93f35fe00b0f7391ac2cf5ec6ec6360dd06e9
CornLiu <74982782+oldcorn26@users.noreply.github.com>
2023-07-22 15:37:22 +0800

    2023/7/22

--- 201-300/297. Serialize and Deserialize Binary Tree/layer_order_traversal.cpp
+++ 201-300/297. Serialize and Deserialize Binary Tree/layer_order_traversal.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <string>
-#include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -19,83 +18,64 @@ struct TreeNode {
 };
 
 class Codec {
-private:
-    int maxLayer = 0;
-    int nodeNum = 0;
-    vector<vector<int>> layerOrder;
-    void layerOrderTraversal(TreeNode *root, int &&idx) {
-        ++nodeNum;
-        if (!root) {
-            if (layerOrder.size() <= idx) layerOrder.push_back({10000});
-            else layerOrder[idx].push_back(10000);
-            return;
-        }
-
-        if (layerOrder.size() <= idx) layerOrder.push_back({root->val});
-        else layerOrder[idx].push_back(root->val);
-        layerOrderTraversal(root->left, idx + 1);
-        layerOrderTraversal(root->right, idx + 1);
-    }
-    void serializeTree(char *treeString) {
-        treeString[0] = maxLayer / 100, treeString[1] = maxLayer % 100;
-        int idx = 1;
-        char minusFlag;
-        for (vector<int> &vec: layerOrder) {
-            treeString[++idx] = (char) (vec.size() / 100 + 40), treeString[++idx] = vec.size() % 100;
-            for (int &i: vec) {
-                if (i == 10000) {
-                    treeString[++idx] = 'n', treeString[++idx] = '\0';
-                    continue;
-                }
-                i < 0 ? (i = -i, minusFlag = 1) : minusFlag = 0;
-                treeString[++idx] = i / 100 + minusFlag * 20;
-                treeString[++idx] = i % 100;
-            }
-        }
-    }
-    void correctTraversal() {
-        nodeNum -= layerOrder[layerOrder.size() - 1].size();
-        layerOrder.erase(layerOrder.end() - 1);
-    }
-    void readString(string &str) {
-        int idx = 1;
-        maxLayer = str[0] * 100 + str[1];
-        layerOrder.resize(maxLayer);
-        for (int i = 0; i < maxLayer; ++i) {
-            layerOrder[i].resize((str[++idx] - 40) * 100 + str[++idx]);
-            for (int &iter: layerOrder[i]) {
-                ++idx;
-                if (str[idx] == 'n') iter = 10000, ++idx;
-                else if (str[idx] >= 20) iter = -(str[idx] - 20) * 100 - str[++idx];
-                else iter = str[idx] * 100 + str[++idx];
-            }
-        }
-    }
-    TreeNode *generateTree(int &&idx, vector<int> &layerIdx) {
-        if (idx >= maxLayer) return nullptr;
-        int &refer = layerOrder[idx][layerIdx[idx]++];
-        if (refer == 10000) return nullptr;
-        TreeNode *res = new TreeNode(refer);
-        res->left = generateTree(idx + 1, layerIdx);
-        res->right = generateTree(idx + 1, layerIdx);
-        return res;
-    }
 public:
+
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        layerOrderTraversal(root, 0), correctTraversal();
-        maxLayer = layerOrder.size();
-        int &&strLen = (nodeNum + maxLayer + 1) * 2;
-        char res[strLen + 1];
-        serializeTree(res);
-        return string(&res[0], &res[strLen]);
+        queue<TreeNode*> q{{root}};
+        string ret{};
+        while (!q.empty()) {
+            int size = q.size();
+            string temp{};
+            while (size-- > 0) {
+                if (q.front()) {
+                    temp += int2str(q.front()->val);
+                    q.emplace(q.front()->left);
+                    q.emplace(q.front()->right);
+                } else {
+                    temp += "null";
+                }
+                q.pop();
+            }
+            ret += temp;
+        }
+        return ret;
     }
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        readString(data);
-        vector<int> layerIdx(maxLayer);
-        return generateTree(0, layerIdx);
+        int dataIdx{4};
+        TreeNode *root{str2Tree(data.substr(0, 4))};
+        queue<TreeNode*> q{{root}};
+        while (!q.empty()) {
+            int size = q.size();
+            while (size-- > 0) {
+                if (q.front()) {
+                    q.front()->left = str2Tree(data.substr(dataIdx, 4));
+                    q.front()->right = str2Tree(data.substr(dataIdx+4, 4));
+                    q.emplace(q.front()->left);
+                    q.emplace(q.front()->right);
+                    dataIdx += 8;
+                }
+                q.pop();
+            }
+        }
+        return root;
+    }
+
+    string int2str(int i) {
+        if (i < 0)
+            i = 5000 - i;
+        string ret{to_string(i)};
+        ret.insert(0, 4-ret.size(), '0');
+        return ret;
+    }
+
+    TreeNode *str2Tree(string &&s) {
+        if (s == "null")
+            return nullptr;
+        int num{stoi(s)};
+        return num > 1000 ? new TreeNode(5000-num) : new TreeNode(num);
     }
 };
 
